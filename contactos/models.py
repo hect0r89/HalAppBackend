66f4b42317d96ba65be66ce1e5179d2b9a9d9245
XCode <xcode@sala6pc19.cic>
2016-11-10 19:39:01 +0100

    starting backend

---
+++ contactos/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+from telefonos.models import Telefono
+
+
+class Contacto(models.Model):
+    first_name = models.CharField(blank=False, null=False, max_length=50)
+    last_name = models.CharField(blank=True, null=True, max_length=50)
+    email = models.EmailField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=150, blank=True, null=True)
+    phone = models.ManyToManyField(Telefono, blank=False, null=False)
+
+
---
+++ telefonos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TelefonosConfig(AppConfig):
+    name = 'telefonos'
