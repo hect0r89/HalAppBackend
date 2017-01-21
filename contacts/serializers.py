cb997c00cbb052834d13ff9ac4f9ae556d51d010
hector <hectordediego89@gmail.com>
2017-01-21 20:38:02 +0100

    Fixed tests

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -20,6 +20,19 @@ class ContactSerializer(serializers.ModelSerializer):
             validated_data['telefono'] = phone
         return Contact.objects.create(**validated_data)
 
+    def update(self, instance, validated_data):
+        telefono = validated_data.pop('telefono', None)
+        if telefono:
+            phone = Phone.objects.get_or_create(**telefono)[0]
+            validated_data['telefono'] = phone
+            instance.telefono = validated_data.get('telefono', instance.telefono)
+        instance.correo = validated_data.get('correo', instance.correo)
+        instance.direccion = validated_data.get('direccion', instance.direccion)
+        instance.nombre = validated_data.get('nombre', instance.nombre)
+        instance.apellidos = validated_data.get('apellidos', instance.apellidos)
+        instance.save()
+        return instance
+
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         ret['telefono'] = PhoneSerializer(instance.telefono).data
--- contacts/tests.py
+++ contacts/tests.py
@@ -12,32 +12,28 @@ URL_PHONE = '/api/1.0/phones/'
 
 class HalAppTest(APITestCase):
     def setUp(self):
-        self.phonec1 = Phone.objects.create(number='111111111', type=1)
-        self.contact1 = Contact.objects.create(first_name='Contacto1', last_name='Contacto1', phone=self.phonec1)
+        self.phonec1 = Phone.objects.create(numero='111111111', tipo=1)
+        self.contact1 = Contact.objects.create(nombre='Contacto1', apellidos='Contacto1', telefono=self.phonec1, color=1)
 
-        self.phonec2 = Phone.objects.create(number='222222222', type=1)
-        self.contact2 = Contact.objects.create(first_name='Contacto2', last_name='Contacto2', phone=self.phonec2)
+        self.phonec2 = Phone.objects.create(numero='222222222', tipo=1)
+        self.contact2 = Contact.objects.create(nombre='Contacto2', apellidos='Contacto2', telefono=self.phonec2, color =1)
 
     def test_list_contacts(self):
         response = self.client.get(URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
 
-    def test_create_contact(self):
-        response = self.client.post(URL, data={'first_name': "Cotacto 3",'last_name': 'Contacto 3', 'address': 'address3', 'email': 'email3@user.com'})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
     def test_create_contact_with_phone(self):
-        response = self.client.post(URL, data={'first_name': "Cotacto 3",'last_name': 'Contacto 3', 'address': 'address3', 'email': 'email3@user.com', 'phone': self.phonec1.pk})
+        response = self.client.post(URL, data={'telefono': "sd",'nombre': "Cotacto 3",'apellidos': 'Contacto 3', 'direccion': 'address3', 'email': 'email3@user.com',  'color':1})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_phone(self):
-        response = self.client.post(URL_PHONE, data={'number': "numperP3C2", 'type': 1})
+        response = self.client.post(URL_PHONE, data={'numero': "numperP3C2", 'tipo': 1})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_update_contact(self):
-        response = self.client.patch(URL+str(self.contact2.pk)+'/', data={'first_name': "Contacto2 Edited",
-                                               'last_name': 'Contacto2 Edited'})
+        response = self.client.patch(URL+str(self.contact2.pk)+'/', data={'nombre': "Contacto2 Edited",
+                                               'apellidos': 'Contacto2 Edited'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_contact(self):
